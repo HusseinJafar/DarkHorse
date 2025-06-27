6b8b6063c056c85a08751d8e22bea7da5c316404
HusseinJafar <silentfrogs9@gmail.com>
2025-06-27 21:02:13 +0300

    The sidebar and official authentication page are working,fully functional

--- FrontEnd/Authentication/Authentication.js
+++ FrontEnd/Authentication/Authentication.js
@@ -1,5 +1,7 @@
 const hamburgerButton=document.getElementById("hamburger");
 
+const closeButton=document.getElementById("close");
+
 const sideBar=document.getElementById("side-bar");
 
 function openSideBar(){
@@ -7,4 +9,11 @@ function openSideBar(){
     sideBar.classList.add("active");
 };
 
+function closeSideBar(){
+
+    sideBar.classList.remove("active");
+}
+
 hamburgerButton.addEventListener("click",openSideBar);
+
+closeButton.addEventListener("click",closeSideBar);
