9b870e23f71c2caa4cff94c0de4dc4e102771ca0
HusseinJafar <silentfrogs9@gmail.com>
2025-07-05 18:14:40 +0300

    working on the search functionality

---
+++ FrontEnd/Search/Search.js
@@ -0,0 +1,25 @@
+//functionality of the side bar
+
+const openBtn = document.getElementById("open");
+
+const closeBtn = document.getElementById("close");
+
+let time = document.getElementById("time");
+
+const now = new Date();
+
+const sideBarContainer = document.querySelector(".side-bar");
+
+function openSideBar() {
+  sideBarContainer.classList.add("active");
+}
+
+function closeSideBar() {
+  sideBarContainer.classList.remove("active");
+}
+
+openBtn.addEventListener("click", openSideBar);
+
+closeBtn.addEventListener("click", closeSideBar);
+
+window.addEventListener("scroll", closeSideBar);
